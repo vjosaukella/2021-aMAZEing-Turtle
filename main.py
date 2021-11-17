be4c2ecf4603b84b141f98a8529f60ecb2d4e529
Mr. Kiedes <akiedes@vtsd.com>
2021-11-17 10:41:52 -0500

    Solution Upload

--- main.py
+++ main.py
@@ -2,20 +2,17 @@
 import turtle as trtl
 import random as rand
 
-#Setup Variables
+#Turtle settings
+painter = trtl.Turtle()
+painter.pencolor(color)
+painter.speed("fastest")
+painter.pensize(5)
+
+# Maze settings
 numSides = 25
 length = 20
 color = "blue"
 doorSize = length * 2
-
-
-#Turtle Settings
-painter = trtl.Turtle()
-painter.pencolor(color)
-#painter.speed("fastest")
-painter.pensize(5)
-
-
 currentSide = 0
 currentSize = length
 beforeDoor = 0
@@ -43,21 +40,28 @@ def drawBarrier(preBarrier):
 
 while(currentSide < numSides):
     if(currentSide >= 4):
+        # Generate random numbers
         getRand()
 
+        # Double check random numbers are valid - if not recalculate
         while abs(beforeDoor - beforeBarrier < length):
             getRand()
 
+        # Door first
         if(beforeDoor < beforeBarrier):
             drawDoor(beforeDoor)
             drawBarrier(beforeBarrier - beforeDoor - length * 2)
             painter.forward(currentSize - beforeBarrier)
 
+        # Barrier first
         else:
             drawBarrier(beforeBarrier)
             drawDoor(beforeDoor - beforeBarrier)
             painter.forward(currentSize - beforeDoor - length * 2)
+
+        # Make sure to turn for the next wall
         painter.left(90)
+    # Increase side number and size of wall
     currentSide += 1
     currentSize += length
 
